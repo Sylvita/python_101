5cd50e3aaca800c253e6dcc3b54f94ea437dc947
Sylvie Dinh <38122167+Sylvita@users.noreply.github.com>
2024-11-13 17:48:42 +0100

    Add files via upload

--- Practicas/iva.py
+++ Practicas/iva.py
@@ -1,1 +1,21 @@
+#Calculadora IVA
 
+print("CALCULADORA IVA")
+print("***************")
+usuario = input("Usuario: ")
+print("\n")
+print("SUMA IVA")
+precio = float(input("Precio sin IVA €: "))
+print("---------------")
+por_iva = float(input("Porcentaje de IVA %: "))
+print("---------------")
+print("RESULTADO:")
+print(f"Precio sin IVA: {precio} €")
+iva = (precio * por_iva)/100
+print(f"IVA: {iva:.2f} €")
+precio_iva = precio + iva
+print(f"Precio con IVA: {precio_iva:.2f} €")
+
+historicos = [usuario, precio, por_iva]
+print("Historicos: Usuario - Precio - Porcentaje IVA")
+print(historicos)
